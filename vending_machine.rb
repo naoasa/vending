5f9c0b73c5b0e936d415e5aed4bc10f0e649177f
naoasa <asaumi1106n@gmail.com>
2024-10-15 23:38:55 +0900

    feat: 自動販売機のクラスと呼び出しを追加

--- vending_machine.rb
+++ vending_machine.rb
@@ -1,1 +1,18 @@
 # frozen_string_literal: true
+
+require "debug"
+
+# 自動販売機の責務
+class VendingMachine
+  def initialize(name)
+    @manufacturer_name = name
+  end
+
+  private
+    def get_manufacturer_name
+      @manufacturer_name
+    end
+end
+
+# 実行
+vending_machine = VendingMachine.new("サントリー")
